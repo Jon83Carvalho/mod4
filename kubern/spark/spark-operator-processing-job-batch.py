58689e50fede14508cd1fc71199a683da6a441da
Jonas83Carvalho <jonas.carvalho@gmail.com>
2021-09-26 19:19:11 -0300

    v3

--- kubern/spark/spark-operator-processing-job-batch.py
+++ kubern/spark/spark-operator-processing-job-batch.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext, SparkConf
 from pyspar.sql import SparkSession
-import order_summary
+import os
 
 aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
