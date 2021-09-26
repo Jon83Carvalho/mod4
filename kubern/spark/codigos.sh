86f5d40646f82876f3e1df5cb17429db3bec0f7b
Jonas83Carvalho <jonas.carvalho@gmail.com>
2021-09-26 20:37:30 -0300

    a

--- kubern/spark/codigos.sh
+++ kubern/spark/codigos.sh
@@ -1,2 +1,2 @@
-kubectl create secret generic aws-credentials --from-literal=aws_access_key_id=AKIA4FAFXNKDR5DEQKUX --from-literal=aws_secret_access_key=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 -n processing
+kubectl create secret generic aws-credentials --from-literal=aws_access_key_id= --from-literal=aws_secret_access_key=thQvrzm5P65OMYWLRCROmUZrOR0lmn9OKJ1KDBnW -n processing
 kubectl apply -f spark/spark-operator.yaml -n processing
