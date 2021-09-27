188b964b988749eb4d1a01aaa3120c4151ce416a
Jonas83Carvalho <jonas.carvalho@gmail.com>
2021-09-26 23:09:34 -0300

    a

--- kubern/spark/spark-operator-processing-job-batch.py
+++ kubern/spark/spark-operator-processing-job-batch.py
@@ -5,14 +5,18 @@ import os
 aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
 
+print("Chegamos até aqui-1")
+
 conf=(
 SparkConf()
-    .set("spark.hadoop.fs.s3a.access.key",aws_access_key_id)
-    .set("spark.hadoop.fs.s3a.secret.key",aws_secret_access_key)
-    .set("spark.hadoop.fs.s3a.fast.upload",True)
-    .set("spark.hadoop.fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
+    .set("spark.hadoop.fs.s3.access.key",aws_access_key_id)
+    .set("spark.hadoop.fs.s3.secret.key",aws_secret_access_key)
+    .set("spark.hadoop.fs.s3.fast.upload",True)
+    #.set("spark.hadoop.fs.s3.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
     .set("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.3")
 )
+
+print("Chegamos até aqui-2")
 
 sc=SparkContext(conf=conf).getOrCreate()
 
@@ -29,7 +33,7 @@ if __name__="__main__":
         .read
         .format("csv")
         .options(header='true',inferSchema='true', delimiter=',')
-        .load("s3a://igticlusterbucket/raw/titanic.csv")
+        .load("s3://igticlusterbucket/raw/titanic.csv")
     )
 
     df.show()
@@ -39,7 +43,7 @@ if __name__="__main__":
     .write
     .mode("overwrite")
     .format("parquet")
-    .save("s3a://igticlusterbucket/process")
+    .save("s3://igticlusterbucket/process")
     )
 
     print("***********")
