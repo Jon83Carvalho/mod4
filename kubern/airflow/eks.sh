0a69f41ec2176e59564cd730a8fab535d39a5e5c
Jonas83Carvalho <jonas.carvalho@gmail.com>
2021-09-30 19:20:23 -0300

    eks

--- kubern/airflow/eks.sh
+++ kubern/airflow/eks.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
 eksctl create cluster --name=igti-cluster --managed --spot --instance-types=m5.xlarge \
-    --nodes=2 --alb-ingress-access --node-private-networking --region=us-east-2 \
+    --nodes=2 --alb-ingress-access --node-private-networking --region=us-east-1 \
     --nodes-min=2 --nodes-max=3 --full-ecr-access --asg-access --nodegroup-name=ng-igticluster
+
+#ariflowenem
+eksctl create cluster --name=igti-cluster-1 --managed --spot --instance-types=m5.xlarge \
+    --nodes=5 --alb-ingress-access --node-private-networking --region=us-east-1a \
+    --nodes-min=3 --nodes-max=6 --full-ecr-access --asg-access --nodegroup-name=ng-igticluster
+#scalling
+eksctl scale nodegroup --cluster igti-cluster-1 --name ng-igticluster  --nodes 5 --nodes-max 6 --nodes-min 3
